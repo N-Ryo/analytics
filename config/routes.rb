43386679299e9aeb2fb1ebe529a24ac4ac0fcb3c
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-14 10:03:28 +0900

    Bundle install and customized devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'reports#index'
+  get 'reports/show'
+  devise_for :sites
 end
