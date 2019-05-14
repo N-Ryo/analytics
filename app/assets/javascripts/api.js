8400ca0a390767c25cdbc5c5a66626e100bd9aaa
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-14 16:24:29 +0900

    Fix some problems and Coding ajax

---
+++ app/assets/javascripts/api.js
@@ -0,0 +1,17 @@
+$.ajax({
+    url: 'http://localhost:3000/',
+    type: 'POST',
+
+    //通信状態に問題がないかどうか
+    success: function(data) {
+
+        //データ取得処理を書く
+        console.log("success");
+    },
+    //通信エラーになった場合の処理
+    error: function(err) {
+
+        //エラー処理を書く
+        console.log('error');
+    }
+});
