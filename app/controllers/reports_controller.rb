fbb1dc663e8b3527b10c95312099f0a0987dc207
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-20 03:18:17 +0900

    Add table to view, and mold pv.url

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,15 @@
 class ReportsController < ApplicationController
+  VALID_SORT_COLUMNS = %w(url created_at)
+  SORT_COLUMNS = %w(asc desc)
   def index
   end
 
   def show
     @site = current_site
-    @pvs = @site.pvs
+    sort = VALID_SORT_COLUMNS.include?(params[:sort]) ? params[:sort] : 'created_at'
+    @direction = SORT_COLUMNS.include?(params[:direction]) ? params[:direction] : 'asc'
+    @pvs = @site.pvs.order(sort + ' ' + @direction)
+    @direction = @direction == 'asc' ? 'desc' : 'asc'
   end
+
 end
