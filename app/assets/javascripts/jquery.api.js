74fc1923acc7711e9acd7fb314882f153b9f9370
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-19 13:25:33 +0900

    Change jsonp

--- app/assets/javascripts/jquery.api.js
+++ app/assets/javascripts/jquery.api.js
@@ -2,7 +2,7 @@ $(function(){
   $.ajax({
     method: 'post',
     url: 'http://localhost:3000/api/v1/pvs',
-    dataType: "json",
+    dataType: "jsonp",
     contentType: "application/json",
     data: JSON.stringify({
         pv: { url: location.href, }
