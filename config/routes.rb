53027d9bcd073c0f0c80481d38243dc9745eabf0
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-20 01:31:43 +0900

    Remove jquery  script

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   devise_scope :site do
     get '/sites/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   devise_for :sites
   mount API::Root => '/api'
-  # namespace :api, { format: 'json' } do
-  #   namespace :v1 do
-  #     resources :pvs, only: [:create, :index]
-  #   end
-  # end
 end
