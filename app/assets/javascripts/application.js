2c32927871bb240c4edb8d2e1bfcb7b9973607dc
N-Ryo <ryo.joy.joy@gmail.com>
2019-05-20 11:01:38 +0900

    Fix production css_compressor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
 //= require jquery
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require rails-ujs
+//= require activestorage
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
